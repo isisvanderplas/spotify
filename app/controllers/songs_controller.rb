bdcb8e0c8a8e542b29b5caed6a86ad3a27cc24cc
Isis van der Plas <isisvanderplas@hotmail.com>
2017-11-12 14:47:44 +0100

    create new path for song & artist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,39 @@ class SongsController < ApplicationController
   def index
     @songs = Song.all
   end
+
   def show
     @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+
+  def create
+    @song = Song.new(song_params)
+
+    if @song.save
+      redirect_to @song
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    if @artist.update_attributes(artist_params)
+      redirect_to @artist
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :duration, :artist)
   end
 end

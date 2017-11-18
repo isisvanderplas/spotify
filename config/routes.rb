c110eb1782f26e52217b06e95527dc4666e87d63
Isis van der Plas <isisvanderplas@hotmail.com>
2017-11-18 22:10:19 +0100

    Add api resource and artists controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,32 +3,25 @@ Rails.application.routes.draw do
   root "artists#index"
 
   get "artists" => "artists#index", as: :artists
-
   get "artists/new" => "artists#new", as: :new_artist
-
   post "artists" => "artists#create"
-
   get "artists/:id" => "artists#show", as: :artist
-
   get "artists/:id/songs/:id" => "songs#show"
-
   get "artists/:id/edit" => "artists#edit", as: :edit_artist
-
   patch "artists/:id" => "artists#update"
-
   delete "artists/:id" => "artists#destroy"
 
   get "songs" => "songs#index", as: :songs
-
   get "songs/new" => "songs#new", as: :new_song
-
   post "songs" => "songs#create"
-
   get "songs/:id" => "songs#show", as: :song
-
   get "songs/:id/edit" => "songs#edit", as: :edit_song
-
   patch "songs/:id" => "songs#update"
-
   delete "songs/:id" => "songs#destroy"
+
+  namespace :api do
+    resources :artists do
+      resources :songs
+    end
+  end
 end
--- spec/factories/songs.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-
-    title        { Faker::Lorem.words(3).join(' ')}
-    duration     { Faker::Number.decimal }
-    artist       { Faker::Lorem.words(2).join(' ')}
-
-end
